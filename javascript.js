9c9802309625e60894c2b1a5136cf3feacba9a84
nicoosommer <nicosomer388@gmail.com>
2023-07-12 21:51:57 -0300

    Funcion round arreglada

--- javascript.js
+++ javascript.js
@@ -6,12 +6,12 @@ function getComputerChoice (){
 
 function round(player,pc){
     if (player===pc){
-        return "It's a tie";
+        return `It's a tie!You both chose ${player}`;
     }
     else if (player==="ROCK" && pc==="PAPER" || player==="PAPER" && pc==="SCISSOR" || player==="SCISSOR" && pc==="ROCK"){
         return `You lose! ${pc} beats ${player}`
     }
-    else if (player==="ROCK"){
-        return "You won"
+    else if (pc==="ROCK" && player==="PAPER" || pc==="PAPER" && player==="SCISSOR" || pc==="SCISSOR" && player==="ROCK"){
+        return `You won! ${player} beats ${pc}`;
     }
 }
