5faac26af8613142700dee5227388c82e41b002c
nicoosommer <nicosomer388@gmail.com>
2023-08-10 21:18:21 -0300

    Iconos y un poco de funcionalidad

--- javascript.js
+++ javascript.js
@@ -1,66 +1,65 @@
-let ops=["ROCK", "PAPER", "SCISSOR"];
-function getComputerChoice (){
+let playerScore = 0;
+let pcScore = 0;
 
-    return ops[Math.floor(Math.random()*ops.length)];
-}
-let playerScore=0;
-let pcScore=0;
-const mostrar1=document.querySelector(".pcPuntos");
-const mostrar2=document.querySelector(".playerPuntos");
-function round(player,pc){
-    if (player===pc){
-        return `This round is a tie!You both chose ${player}`;
-    }
-    else if (player==="ROCK" && pc==="PAPER" || player==="PAPER" && pc==="SCISSOR" || player==="SCISSOR" && pc==="ROCK"){
-        pcScore++;
-        return `You lost this round! ${pc} beats ${player}`
-    }
-    else if (pc==="ROCK" && player==="PAPER" || pc==="PAPER" && player==="SCISSOR" || pc==="SCISSOR" && player==="ROCK"){
-        playerScore++;
-        return `You won this round! ${player} beats ${pc}`;
-    }
-    else{
-        return "You enter an invalid option! Try again";
-    }
-    
+const rock = document.getElementById("ROCK");
+const paper = document.getElementById("PAPER");
+const scissor = document.getElementById("SCISSOR");
+const resultado = document.querySelector(".resultado");
+const ops = ["ROCK", "PAPER", "SCISSOR"];
+const pcPuntos = document.querySelector(".pcPuntos");
+const playerPuntos = document.querySelector(".playerPuntos");
+const restart = document.getElementById("restart");
+
+pcPuntos.textContent = 0;
+playerPuntos.textContent = 0;
+function getComputerChoice() {
+  return ops[Math.floor(Math.random() * ops.length)];
 }
 
-function ganador(){
-    if (pcScore<5 && playerScore<5){    
-        mostrar1.textContent=pcScore;
-        mostrar2.textContent=playerScore;
-    }
-    else{
-        if(playerScore>pcScore){
-            div.textContent=`Con un resultado de ${playerScore} a ${pcScore}, usted es el ganador`;
-        }
-        else{
-            div.textContent=`Con un resultado de ${pcScore} a ${playerScore}, la maquina es la ganadora`;
-        }
-        
-    }
+function playRound(player, pc) {
+  if (player === pc) {
+    resultado.textContent = `This round is a tie!You both chose ${player}`;
+  } else if (
+    (player === "ROCK" && pc === "PAPER") ||
+    (player === "PAPER" && pc === "SCISSOR") ||
+    (player === "SCISSOR" && pc === "ROCK")
+  ) {
+    pcScore++;
+    pcPuntos.textContent = pcScore;
+    resultado.textContent = `You lost this round! ${pc} beats ${player}`;
+  } else {
+    playerScore++;
+    playerPuntos.textContent = playerScore;
+    resultado.textContent = `You won this round! ${player} beats ${pc}`;
+  }
+  gameFinished(playerScore, pcScore);
 }
 
-const div=document.querySelector(".resultado");
+function gameFinished(player, pc) {
+  if (player == 5 || pc == 5) {
+    pcPuntos.textContent = 0;
+    playerPuntos.textContent = 0;
+    playerScore = 0;
+    pcScore = 0;
+    if (player > pc) {
+      resultado.textContent = `You win! ${player} to ${pc}`;
+    } else {
+      resultado.textContent = `The pc wins! ${pc} to ${player}`;
+    }
+  }
+}
+function restartGame() {
+  pcPuntos.textContent = "";
+  playerPuntos.textContent = "";
+  resultado.textContent = "";
+}
 
-const rock=document.getElementById("rock");
-rock.addEventListener("click",()=>{
-    div.textContent=round("ROCK",getComputerChoice());
-    // mostrar1.textContent=pcScore;
-    // mostrar2.textContent=playerScore;
-    ganador()
-})
+function userChoice(e) {
+  const eleccionJug = e.target.id;
+  console.log(playRound(eleccionJug, getComputerChoice()));
+}
 
-const paper=document.getElementById("paper");
-paper.addEventListener("click",()=>{
-    div.textContent=round("PAPER",getComputerChoice());
-   ganador()
-})
-
-const scissor=document.getElementById("scissor");
-scissor.addEventListener("click",()=>{
-    div.textContent=round("SCISSOR",getComputerChoice());
-    // mostrar1.textContent=pcScore;
-    // mostrar2.textContent=playerScore;
-    ganador()
-})
+rock.addEventListener("click", userChoice);
+paper.addEventListener("click", userChoice);
+scissor.addEventListener("click", userChoice);
+restart.addEventListener("click", restartGame);
